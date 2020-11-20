66ce7970b2a43bb515251f805287b3306ca87307
Nathan Schmitzer <nathan.schmitzer@yahoo.com>
2020-11-19 18:38:17 -0600

    added routes to stats page and updating workout page

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -24,4 +24,33 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+      {
+        _id: params.id,
+      },
+      {
+        $push: {
+          exercises: body,
+        },
+      }
+    )
+      .then((updatedWorkout) => {
+        res.json(updatedWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
 module.exports = router;
--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html")).catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))  
 });
